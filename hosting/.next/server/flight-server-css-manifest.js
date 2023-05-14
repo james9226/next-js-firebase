52a2b9aa6fc9329f8c49c3426029685ab1b26d6c
James Neale <42097394+james9226@users.noreply.github.com>
2023-05-14 22:44:50 +0100

    fix

---
+++ hosting/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"cssImports":{},"cssModules":{"/Users/james/dev/test-nextjs/hosting/app/page":[],"/Users/james/dev/test-nextjs/hosting/app/day/page":[],"/Users/james/dev/test-nextjs/hosting/app/about/page":[]}}
